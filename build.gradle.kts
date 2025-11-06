4da89a99ee1ae27e831d31b61bd131adfeb931de
Kim, Joon <kjoon418@naver.com>
2025-11-06 16:50:53 +0900

    build: SLF4J 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,13 @@ repositories {
 }
 
 dependencies {
+    // Tomcat
     implementation("org.apache.tomcat.embed:tomcat-embed-core:11.0.12")
     implementation("org.apache.tomcat.embed:tomcat-embed-logging-juli:10.1.30")
+
+    // SLF4J
+    implementation("org.slf4j:slf4j-api:2.0.13")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 
     testImplementation(kotlin("test"))
 }
