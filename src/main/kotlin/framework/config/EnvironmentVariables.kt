3f3493699de86a31e81a2f8affe7a9e5dea18dfd
Kim, Joon <kjoon418@naver.com>
2025-11-08 17:09:05 +0900

    fix: 커밋에 누락된 수정 포함

--- src/main/kotlin/framework/config/EnvironmentVariables.kt
+++ src/main/kotlin/framework/config/EnvironmentVariables.kt
@@ -1,4 +1,6 @@
 package framework.config
 
+val BASE_PACKAGES = listOf("framework", "mocker")
+const val CHARACTER_ENCODING = "UTF-8"
 const val SERVER_NAME = "WatsonServer"
 const val SERVER_BASE_DIR_PATH = "tomcat-temp"
