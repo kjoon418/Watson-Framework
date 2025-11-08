d55a2ab9d5cbae3bd178148599e182ea08b3d5e3
Kim, Joon <kjoon418@naver.com>
2025-11-08 18:00:38 +0900

    chore(exception): 예외를 로그에 출력하도록 수정

--- src/main/kotlin/framework/exception/ExceptionResponseHandler.kt
+++ src/main/kotlin/framework/exception/ExceptionResponseHandler.kt
@@ -1,10 +1,15 @@
 package framework.exception
 
 import framework.dto.Response
+import org.slf4j.LoggerFactory
 import kotlin.Exception
 
 class ExceptionResponseHandler {
+    private val log = LoggerFactory.getLogger(ExceptionResponseHandler::class.java)
+
     fun createErrorResponse(exception: Exception): Response {
+        log.warn(exception.stackTraceToString())
+
         return Response(
             code = exception.statusCode,
             contentType = "text/plain",
