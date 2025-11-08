8eba083772ab83787223864d0c92758f1e019160
Kim, Joon <kjoon418@naver.com>
2025-11-08 17:10:31 +0900

    feat(dispatcher): `Dispatcher` 구현

---
+++ src/main/kotlin/framework/dispatcher/Dispatcher.kt
@@ -0,0 +1,52 @@
+package framework.dispatcher
+
+import framework.action.Action
+import framework.action.ActionResponse
+import framework.dto.Request
+import framework.dto.Response
+import framework.exception.ExceptionResponseHandler
+import framework.request.HttpBodyMapper
+import framework.response.ResponseMapper
+import framework.router.PathVariableExtractor
+import framework.router.Router
+import java.lang.reflect.ParameterizedType
+
+class Dispatcher(
+    private val responseMapper: ResponseMapper,
+    private val bodyMapper: HttpBodyMapper,
+    private val router: Router,
+    private val pathVariableExtractor: PathVariableExtractor,
+    private val exceptionResponseHandler: ExceptionResponseHandler
+) {
+    fun dispatch(request: Request): Response {
+        try {
+            val (routeKey, action) = router.route(request)
+            val type = findTypeOf(action)
+            val body = bodyMapper.map(request.requestBody.body, type)
+            val pathVariables = pathVariableExtractor.extract(routeKey, request.requestContext.path)
+
+            val actionResponse = runAction(
+                action = action,
+                request = request,
+                pathVariables = pathVariables,
+                body = body
+            )
+
+            return responseMapper.map(actionResponse)
+        } catch (exception: Exception) {
+            return exceptionResponseHandler.createErrorResponse(exception)
+        }
+    }
+
+    private fun findTypeOf(action: Action<*, *>): Class<*> {
+        val superclass = action.javaClass.genericInterfaces
+            .firstOrNull { it is ParameterizedType } as? ParameterizedType
+
+        return superclass?.actualTypeArguments?.first() as Class<*>
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun runAction(action: Action<*, *>, request: Request, pathVariables: Map<String, String>, body: Any): ActionResponse<*> {
+        return (action as Action<Any?, *>).act(request, pathVariables, body)
+    }
+}
