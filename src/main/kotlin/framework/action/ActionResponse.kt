d375a1cf67cccedf461b47e99245088be539c066
Kim, Joon <kjoon418@naver.com>
2025-11-11 13:55:23 +0900

    chore(action): `ActionResponse`에 NO CONTENT에 대한 정적 팩터리 메서드 추가

--- src/main/kotlin/framework/action/ActionResponse.kt
+++ src/main/kotlin/framework/action/ActionResponse.kt
@@ -5,4 +5,13 @@ import framework.constants.HttpStatus
 data class ActionResponse<out R>(
     val status: HttpStatus = HttpStatus.OK,
     val body: R? = null
-)
+) {
+    companion object {
+        fun noContent(): ActionResponse<Unit> {
+            return ActionResponse(
+                status = HttpStatus.NO_CONTENT,
+                body = Unit
+            )
+        }
+    }
+}
