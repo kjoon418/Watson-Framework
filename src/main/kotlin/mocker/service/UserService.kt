6fccb417aae40662c139b43f6ce82c5c8a14b06c
Kim, Joon <kjoon418@naver.com>
2025-11-14 15:38:40 +0900

    refactor(mocker): `UserService`도 인터페이스 기반으로 리팩터링

--- src/main/kotlin/mocker/service/UserService.kt
+++ src/main/kotlin/mocker/service/UserService.kt
@@ -1,35 +1,11 @@
 package mocker.service
 
-import framework.exception.IllegalRequestException
-import framework.security.Role
-import framework.security.createToken
-import framework.component.Component
-import framework.security.PasswordEncoder
-import mocker.dto.response.TokenDto
 import mocker.dto.request.UserSignInRequestDto
 import mocker.dto.request.UserSignUpRequestDto
-import mocker.entity.User
-import mocker.repository.userRepository
+import mocker.dto.response.TokenDto
 
-@Component
-class UserService {
-    fun signUp(requestDto: UserSignUpRequestDto): TokenDto {
-        val user = User(
-            key = requestDto.key,
-            id = requestDto.id,
-            password = PasswordEncoder.encode(requestDto.password)
-        )
+interface UserService {
+    fun signUp(requestDto: UserSignUpRequestDto): TokenDto
 
-        userRepository.save(user)
-
-        return TokenDto(createToken(user.key, listOf(Role.USER)))
-    }
-
-    fun signIn(requestDto: UserSignInRequestDto): TokenDto {
-        val user = userRepository.findAll()
-            .firstOrNull { it.id == requestDto.id && PasswordEncoder.isMatch(requestDto.password, it.password) }
-            ?: throw IllegalRequestException("부적절한 아이디 혹은 비밀번호입니다.")
-
-        return TokenDto(createToken(user.key, listOf(Role.USER)))
-    }
+    fun signIn(requestDto: UserSignInRequestDto): TokenDto
 }
