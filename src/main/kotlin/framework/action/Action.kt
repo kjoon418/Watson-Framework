f0024dab297199e870d883764953baf0ae72974b
Kim, Joon <kjoon418@naver.com>
2025-11-14 21:13:56 +0900

    fix(action): 불필요한 속성 제거

--- src/main/kotlin/framework/action/Action.kt
+++ src/main/kotlin/framework/action/Action.kt
@@ -2,9 +2,6 @@ package framework.action
 
 import framework.dto.Request
 
-const val METHOD_NAME = "act"
-const val PARAMETER_COUNT = 3
-
 interface Action<in P, out R> {
     fun act(
         request: Request,
